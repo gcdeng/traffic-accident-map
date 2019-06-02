5a00eb71fe2dd74e0226e110529af2dadbd49005
gcdeng <ericdenggc@gmail.com>
2019-06-02 18:02:02 +0800

    add marker and infowindow

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { compose, withProps, withHandlers, withStateHandlers } from "recompose";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps";
+import { MarkerClusterer } from "react-google-maps/lib/components/addons/MarkerClusterer";
+import {sampleData} from './data';
+import {apiKey} from './const';
+
+// console.log(sampleData);
+
+const googleMapURL = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL,
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `100vh` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withHandlers({
+    onMarkerClustererClick: () => (markerClusterer) => {
+      // const clickedMarkers = markerClusterer.getMarkers()
+      // console.log(`Current clicked markers length: ${clickedMarkers.length}`)
+      // console.log(clickedMarkers)
+    },
+    onMarkerClick: () => (data) => {
+      // console.log(data);
+      
+    },
+  }),
+  withStateHandlers(() => ({
+      isOpen: {},
+    }), {
+      onToggleOpen: ({isOpen}) => (i) => ({
+        isOpen: {
+          ...isOpen,
+          [i]: !isOpen[i]
+        }
+      })
+    }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: sampleData[0]['緯度'], lng: sampleData[0]['經度'] }}
+  >
+    <MarkerClusterer
+      averageCenter
+      enableRetinaIcons
+      gridSize={60}
+    >
+      {props.isMarkerShown && 
+      sampleData.map((data, i) => {
+        if(typeof data['緯度'] === 'number' && typeof data['經度'] === 'number'){
+          return <Marker
+            key={i}
+            position={{ lat: data['緯度'], lng: data['經度'] }}
+            onClick={() => props.onToggleOpen(i)}
+            >
+              {props.isOpen[i] && <InfoWindow onCloseClick={() => props.onToggleOpen(i)}>
+                <div>
+                  <div>{data['發生地點']}</div>
+                  <div>{data['死亡受傷人數']}</div>
+                  <div>{data['車種']}</div>
+                  <div>{data['發生時間']}</div>
+                </div>
+              </InfoWindow>}
+            </Marker>
+        }
+      })}
+    </MarkerClusterer>
+  </GoogleMap>
+)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyMapComponent
+        isMarkerShown
+      />
     </div>
   );
 }
---
+++ src/const.js
@@ -0,0 +1,5 @@
+const apiKey = 'AIzaSyBJp5MxlXMvUGfMhc81-hw0UAloieWzgmU';
+
+module.exports = {
+    apiKey
+}
