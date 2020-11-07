4ae7c04ce5ce01d5dd775be400f32aa1c631d33c
gcdeng <ericdenggc@gmail.com>
2020-11-07 18:15:04 +0800

    fix path

--- server/util/importCsvToDb.js
+++ server/util/importCsvToDb.js
@@ -6,10 +6,10 @@ const initDb = require('../models/db');
 const db = initDb();
 const LocationModel = require('../models/location');
 let importCsvToDb = async () => {
-    let csvFiles = fs.readdirSync(path.join(__dirname, './csv/'));
+    let csvFiles = fs.readdirSync(path.join(__dirname, '../csv/'));
     for (let index = 1; index < csvFiles.length; index++) {
         let csvFileName = csvFiles[index];
-        let csvFilePath = path.join(__dirname, './csv/'+csvFileName);
+        let csvFilePath = path.join(__dirname, '../csv/'+csvFileName);
         console.log(csvFilePath);
         let jsonArray = await csv().fromFile(csvFilePath);
         let data = await LocationModel.insertMany(jsonArray);
